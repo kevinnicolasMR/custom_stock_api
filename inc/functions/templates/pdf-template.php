28d1a05fc6610fabe9d19fa8a673b0ad7d3acb8a
kevinnicolasMR <kevinnicolasmr@gmail.com>
2024-11-10 13:18:26 -0300

    PDF file cambios

--- inc/functions/templates/pdf-template.php
+++ inc/functions/templates/pdf-template.php
@@ -3,7 +3,8 @@ function render_pdf_template($file) {
     ob_start();
     ?>
     <div class="file-item file-item-pdf filter-prop-element" alt="<?php echo esc_attr($file->name); ?>">
-        <p><?php echo esc_html($file->name); ?> <a href="https://drive.google.com/file/d/<?php echo esc_attr($file->id); ?>/view" target="_blank">Ver PDF</a></p>
+        <p><?php echo esc_html($file->name); ?></p>
+        <p><a href="https://drive.google.com/file/d/<?php echo esc_attr($file->id); ?>/view" target="_blank">Ver PDF</a></p>
     </div>
     <?php
     return ob_get_clean();
